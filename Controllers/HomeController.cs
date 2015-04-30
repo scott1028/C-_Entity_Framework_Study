a7f26f6e2cb4acc778731dae69a11fe1594885d7
scott <mic1028002@gmail.com>
2015-04-30 08:34:07 +0800

    fm

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace myTestMVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index ()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName ();
+			var isMono = Type.GetType ("Mono.Runtime") != null;
+
+			ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData ["Runtime"] = isMono ? "Mono" : ".NET";
+
+			using (var db = new DaoContext ()) {
+				Console.WriteLine (
+					db.Users.First ().ToString ()
+				);
+				ViewData ["Runtime"] = db.Users.First ().user_id;
+			}
+
+			return View ();
+		}
+	}
+}
+
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace myTestMVC
+{
+	public partial class DaoContext : DbContext
+	{
+		public DaoContext():base("name=SYS_DB")
+		{
+		}
+
+		public virtual DbSet<User> Users { get; set; }
+	}
+
+	[Table("user")]
+	public class User
+	{
+		public int id { get; set;}
+		public string user_id { get; set; }
+		public string password { get; set; }
+	}
+}
+
